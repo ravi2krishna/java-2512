fd47d5b7d0f33f9ebc4c3fec6839aa00086d58b7
ravi2krishna <ravi2krishna@gmail.com>
2025-09-10 12:04:44 +0530

    Data Types

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        int value = 10;
+        String data = "Java";
+        // Student john = "John is from USA"; // in future you can create own data type
+
+        // byte bigNumber = 10000; // byte cannot hold 10000
+        short bigNumber = 10000;
+        System.out.println(value);
+        System.out.println(data);
+        System.out.println(bigNumber);
+
+    }
+}
