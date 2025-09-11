bc9aac5b749d70401b5b553f3fcc6c249aa2b487
ravi2krishna <ravi2krishna@gmail.com>
2025-09-11 11:59:37 +0530

    Concatenation

--- DataTypes.java
+++ DataTypes.java
@@ -11,5 +11,46 @@ public class DataTypes {
         System.out.println(data);
         System.out.println(bigNumber);
 
+        // all primitive types
+        byte b = 9;
+        short s = 999;
+        int i = 99999;
+        long l = 9999999;
+
+        float f = 3.14f; // error: incompatible types
+        double d = 32.7818191;
+
+        char c = 'c';
+        boolean bool = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+
+        System.out.println(f);
+        System.out.println(d);
+
+        System.out.println(c);
+        System.out.println(bool);
+
+        // non primitive type
+        String greet = "Good Morning "; // --> String
+        String name = "Ravi "; 
+        byte time = 11;
+
+        // Employees ID Multiple values  --> Array 
+        int [] ids = {101,102,103}; 
+
+        // Concatenation
+        System.out.println(greet + name + time);
+        System.out.println("Class will end by: " +time+1);
+        System.out.println("Class will end by: " +(time+1));
+
+        int num1 = 10;
+        int num2 = 20;
+
+        System.err.println(num1+num2); // 1020 or 30
+
     }
 }
---
+++ Operators.java
@@ -0,0 +1,73 @@
+public class Operators {
+    public static void main(String[] args) {
+        // Arithmetic Operators
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println("Sum Of num1 and num2: "+(num1+num2));
+        System.out.println("Diff Of num1 and num2: "+(num1-num2));
+        System.out.println("Product Of num1 and num2: "+(num1*num2));
+        System.out.println("Division Of num1 and num2: "+(num1/num2));
+        System.out.println("Modulus Of num1 and num2: "+(num1%num2));
+
+
+        // Increment / Decrement Operators
+        int count = 1;
+        count++;
+        System.out.println(count);
+
+        int newCount = 10;
+        newCount--;
+        System.out.println(newCount);
+
+        // Compound Assignment Operators
+        int x = 10;
+        x = x + 5;
+        System.out.println(x);
+
+        int y = 10;
+        y += 5;
+        System.out.println(y); // 15
+
+        y *= 5; // 15 * 5
+        System.out.println(y); // 15, 50, 75,  
+
+
+        // Relational / Comparison Operators
+        int n1 = 10;
+        int n2 = 5;
+
+        System.out.println( n1 == n2);
+        System.out.println( n1 != n2);
+        System.out.println( n1 > n2);
+
+        // Logical Operators
+        int a = 10;
+        int b = 5;
+        int c = 3;
+        int d = 2;
+        System.out.println("Logical Operators");
+        System.out.println(a > b && c < d); // T && F -> F
+        System.out.println(a > b && c > d); // T && T -> T
+
+        System.out.println(a > b || c < d); // T // F -> T
+        System.out.println(a < b || c < d); // F // F -> F
+
+        System.out.println(! (a > b)); 
+        boolean result = a > b;
+        System.err.println(result);
+        System.err.println(! result);
+
+        // Bitwise Operators
+        int b1 = 5; // 0000000000000101
+        int b2 = 3; // 0000000000000011
+                    // 0000000000000111
+                    // 0000000000000001 &
+
+        System.err.println(b1 & b2); // 1
+        System.err.println(b1 | b2); // 7
+
+
+
+    }
+}
