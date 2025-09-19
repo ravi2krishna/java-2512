f4a4b4759fc57284db9b420207e2502a1c167d15
ravi2krishna <ravi2krishna@gmail.com>
2025-09-19 12:16:51 +0530

    constructor overloading with current and savings

--- seven_constructor/Bank.java
+++ seven_constructor/Bank.java
@@ -6,10 +6,13 @@ public class Bank {
     // if you want to create an account in HDFC Bank 
     // Account Can be Opened if minimum balance is 10000
 
+    // now we can accept bank account with custom balance 2000, 5000, 0
+
+
     // initial account creation
     double minBalance;
 
-    // bank start up operations
+    // bank start up operations -> current account
     public Bank() {
         System.out.println("======== HDFC ========");
         System.out.println("Creating HDFC Account");
@@ -19,15 +22,20 @@ public class Bank {
         minBalance-=1500;
     }
 
+    // savings account
+    public Bank(double minBalance) {
+        this.minBalance = minBalance;
+    }
+
     public double showBalance() {
         return minBalance;
     }
 
     public static void main(String[] args) {
-        Bank customer_one = new Bank();
+        Bank customer_one = new Bank(); // current account 
         System.out.println(customer_one.showBalance());
 
-        Bank customer_two = new Bank();
+        Bank customer_two = new Bank(2000); // savings account
         System.out.println(customer_two.showBalance());
         
     }
